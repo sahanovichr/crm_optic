4d61e33e2b53aaf238e5c2b785727d5adbbf6760
sahanovichr <sahanovichr@gmail.com>
2025-08-21 21:01:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-# Flask app will be here
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# временное хранилище (для теста)
+clients = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', clients=clients)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_client():
+    if request.method == 'POST':
+        client = {
+            'name': request.form['name'],
+            'phone': request.form['phone'],
+            'birthday': request.form['birthday']
+        }
+        clients.append(client)
+        return redirect(url_for('index'))
+    return render_template('add_client.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)# Flask app will be here
